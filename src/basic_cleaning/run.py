c11801dfe7d478a9cbbcc729a724d7e2ed9771dd
Tessa van der Heiden <tessavanderheiden@Tessas-MacBook-Pro.local>
2022-11-29 15:23:34 +0100

    modified cleaning step for ranges

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -35,6 +35,10 @@ def go(args):
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    # Only include values between correct range
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     df.to_csv("clean_sample.csv", index=False)
 
     logger.info("Creating artifact")
